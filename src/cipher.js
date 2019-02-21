32a8a927be063d20ba105e779eed818f2431beef
lesashley <lesashley56@gmail.com>
2019-02-21 12:45:37 -0500

    working on the cypher function

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,32 @@
 // window.cipher = {
-//   encode: () => {
+//   encode: (offset,string) => {
 //     /* Acá va tu código */
 //   },
 //   decode: () => {
 //     /* Acá va tu código */
 //   }
 // };
+
+
+const offSet1=document.getElementById("offset1");
+const offSet2=document.getElementById("offset2");
+const firstPassword1=document.getElementById("firstpassword1");
+const firstPassword2=document.getElementById("firstpassword2");
+window.cipher ={
+encode :(offset1,firstpassword1) =>{
+    var arr=[];
+     for ( let i=0;i<firstpassword1.length;i++ ) {
+    if( firstPassword1.chardCodeAt(i)>=65 && firstPassword1.charCodeAt(i)<=90)
+     arr.push(firstPassword1.fromCharCode(firstPassword1.ChardCodeAt(i)+offSet1));
+    }
+},
+
+ decode :(offset2,firstpassword2) =>{
+    var arr=[];
+    for ( let i=0;i<firstpassword2.length;i++ ) {
+   if( firstPassword2.chardCodeAt(i)>=65 && firstPassword2.charCodeAt(i)<=90)
+    arr.push(firstPassword2.fromCharCode(firstPassword2.ChardCodeAt(i)+offSet2));
+   }
+
+}
+}
--- src/index.js
+++ src/index.js
@@ -3,26 +3,19 @@ const firstScreen=document.getElementById("firstScreen");
 const optionsScreen=document.getElementById("optionsScreen");
 const encodeScreen=document.getElementById("encodeScreen");
 const decodeScreen=document.getElementById("decodeScreen");
-
-
 optionsScreen.style.display="none";
 encodeScreen.style.display="none";
 decodeScreen.style.display="none";
-
 const createAnAccount=document.getElementById("createAnAccount");
-
-
 createAnAccount.addEventListener('click', function() {
     firstScreen.style.display = 'none';
     optionsScreen.style.display = "block"; 
 })
-
 const encode=document.getElementById("encode");
 encode.addEventListener('click', function() {
     optionsScreen.style.display = 'none';
     encodeScreen.style.display = "block"; 
 })
-
 const decode=document.getElementById("decode");
 decode.addEventListener('click', function() {
     optionsScreen.style.display = 'none';
