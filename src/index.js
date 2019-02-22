59d5466003f7943d7540f956ce4c298e99c22093
maytezhou <maytezhou14@gmail.com>
2019-02-22 16:11:58 -0500

    doing some adjusments to html and CSS

--- src/index.js
+++ src/index.js
@@ -1,48 +1,37 @@
-const encodeButton2 =  document.getElementById('encodebutton2');
-encodeButton2.addEventListener('click', function(){
-  const passwordTest1= document.getElementById('passwordtest1').value;
-  const offSet1 = parseInt(document.getElementById('offset1').value);
-   console.log(encode(passwordTest1,offSet1));
+
+
+  const firstScreen=document.getElementById('firstscreen');
+  const optionsScreen=document.getElementById('optionsscreen');
+  const encodeScreen=document.getElementById('encodeScreen');
+  const decodeScreen=document.getElementById('decodescreen');
   
-  //console.log(typeof(passwordTest1);
-  //console.log(typeof (offSet1));
- 
- });
-
-
-
- function encode (string, offset) {
-    const assciNumber = string.charCodeAt();
-    const newAscci = (assciNumber - 65 + offset) % 26 + 65;
-    const newLetter = String.fromCharCode(newAscii)
-    return newLetter;
-  }
   
+  
+  optionsScreen.style.display='none';
+  encodeScreen.style.display='none';
+  decodeScreen.style.display='none';
 
-  const encodeScreen=document.getElementById("encodeScreen");
-  const decodeScreen=document.getElementById("decodescreen");
-  encodeScreen.style.display="none";
-  decodeScreen.style.display="none";
 
-const createAnAccount=document.getElementById("createAnAccount");
+const createAnAccount=document.getElementById('createanaccount');
+
 createAnAccount.addEventListener('click', function() {
     firstScreen.style.display = 'none';
-    optionsScreen.style.display = "block"; 
-})
+    optionsScreen.style.display = 'block'; 
+});
 
-const encodeButton1=document.getElementById("encodebutton1");
+const encodeButton1=document.getElementById('encodebutton1');
 encodeButton1.addEventListener('click', function() {
     optionsScreen.style.display = 'none';
-    encodeScreen.style.display = "block"; 
-})
-const decodeButton1=document.getElementById("decodebutton1");
+    encodeScreen.style.display = 'block'; 
+});
+const decodeButton1=document.getElementById('decodebutton1');
 decodeButton1.addEventListener('click', function() {
     optionsScreen.style.display = 'none';
-    decodeScreen.style.display = "block"; 
-})
+    decodeScreen.style.display = 'block'; 
+});
 
 //calling cypher.encode 
-const encode1=document.getElementById("encode1");
+const encode1=document.getElementById('encode1');
 encode1.addEventListener('click', function() {
     return cipher.encode();
 })
